aa56ba045215f5f9b0326ee0793afd08c2c04943
Chuck Lu <chuck.lu@qq.com>
2020-11-18 19:47:45 +0800

    add new project to use https://docs.microsoft.com/en-us/dotnet/api/system.security.cryptography.rsa.importrsapublickey?view=netcore-3.1

---
+++ ConsoleAppRsa/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ConsoleAppRsa
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Hello World!");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.ReadLine();
+            }
+        }
+    }
+}
